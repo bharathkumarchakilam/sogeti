f75bc506efa5653d5d2e4997eb13d7277e8c8479
bharathkumarchakilam <bharathchakilam2137@gmail.com>
2025-01-23 16:24:31 +0530

    ....

--- fun.py
+++ fun.py
@@ -1,31 +1,31 @@
 #Square Root (math.sqrt)
 import math
-number = 16
+number = 36
 result = math.sqrt(number)
-print("Square root of 16:", result)
+print("Square root of 36:", result)
 
 #Power (math.pow):
-base = 2
+base = 3
 exponent = 3
 result = math.pow(base, exponent)
-print("2 raised to the power 3:", result)
+print("3 raised to the power 3:", result)
 
 #Absolute Value (abs):
-number = -7
+number = -9
 result = abs(number)
-print("Absolute value of -7:", result)
+print("Absolute value of -9:", result)
 
 #2. Trigonometric Functions
 # Sine (math.sin):
 import math
-angle = math.radians(30)
+angle = math.radians(45)
 result = math.sin(angle)
-print("Sine of 30 degrees:", result)
+print("Sine of 45 degrees:", result)
 
 # Cosine (math.cos):
-angle = math.radians(60)
+angle = math.radians(45)
 result = math.cos(angle)
-print("Cosine of 60 degrees:", result)
+print("Cosine of 45 degrees:", result)
 
 #Tangent (math.tan):
 angle = math.radians(45)
@@ -39,8 +39,8 @@ result = math.log(number)
 print("Natural logarithm of 10:", result)
 
 #Logarithm with Base 10 (math.log10):
-result = math.log10(100)
-print("Logarithm base 10 of 100:", result)
+result = math.log10(1000)
+print("Logarithm base 10 of 1000:", result)
 
 #4. Constants
 #Value of Pi (math.pi):
@@ -50,10 +50,25 @@ print("Value of Pi:", math.pi)
 #Value of Euler's Number (math.e):
 print("Value of Euler's number (e):", math.e)
 
-number = 4.2
+#5.Rounding Functions
+#Ceiling (`math.ceil`):
+number = 5.6
 result = math.ceil(number)
-print("Ceiling of 4.2:", result)
+print("Ceiling of 5.6:", result)
 
-number = 4.8
+#Flooring ('math.floor'):
+number = 8.9
 result = math.floor(number)
-print("Floor of 4.8:", result)
+print("Floor of 8.9:", result)
+
+#6. Factorial
+#Factorial (`math.factorial`):
+number = 10
+result = math.factorial(number)
+print("Factorial of 10:", result)
+
+#7. GCD (Greatest Common Divisor)
+#GCD (`math.gcd`):
+result = math.gcd(2, 6)
+print("GCD of 2 and 6:", result)
+
